bb0c667fcaa37e21967e3d023acc9aec8623a17d
Cody M <mathrox_3.14@hotmail.com>
2016-12-12 02:00:31 -0500

    Added some code for the visual representation

--- dijkstra.rkt
+++ dijkstra.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+(require racket/draw)
 
 (define (dijkstra origin destination graph)
   (define (update x)
@@ -30,3 +30,15 @@
           (modify-previous! (vector-ref i cont) a))
         (update-previous-dist-node!-aux i n d a (+ cont 1))))
   (update-previous-dist-node!-aux i n d a 0))
+
+
+
+
+
+(define target (make-bitmap 200 200))
+(define dc (new bitmap-dc% [bitmap target]))
+
+(for ([i 10])
+  (for ([j 10])
+    (send dc set-brush "black" 'solid)
+    (send dc draw-rectangle (+ 0 (* 10 i) 1) (+ 0 (* 10 j) 1) 10 10))) 
